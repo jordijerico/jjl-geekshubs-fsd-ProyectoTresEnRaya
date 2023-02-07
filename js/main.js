22aa7a1ff3c150a6a4135e644479d6285d31660c
Jordi Jerico <jordijerico@gmail.com>
2023-02-07 12:26:41 +0100

    Implementado cierto JS para controlar turnos, y matriz de combinaciones ganadoras en JS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
-let tablero = Array.from(document.getElementsByClassName("boxes"));
 
-let turno = true;
-tablero.map(
+let logicBoard = Array.from(document.getElementsByClassName("boxes"));
+
+let turn = true;
+
+let turnPlayer1 = 3;
+let turnPlayer2 = 3;
+
+
+let boardGame = ["", "", "", "", "", "", "", "", "",];
+
+let chartWinner = [
+
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+logicBoard.map(
     (box) => {
+        // Evento / funcion que comprueba los turnos y va pintando X o O cambiando el turno y bajando los turnos
         box.addEventListener("click", () => {
-            if (box.innerHTML === "") {
-                box.innerHTML = (turno) ? "X" : "O";
+            if ((box.innerHTML === "") && (turnPlayer1 > 0 || turnPlayer2 > 0)) {
+                box.innerHTML = (turn) ? "X" : "O";
+
+                if (turn) {
+                    turnPlayer1--;
+                } else {
+                    turnPlayer2--;
+                }
 
                 //Cambiamos turno
-                turno = !turno;
+                turn = !turn;
             }
         })
     }
