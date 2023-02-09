f81f3c9aa80ae275e6248d0194e2a01eff0dc13a
jordijerico <jordijerico@gmail.com>
2023-02-09 16:47:25 +0100

    Añadido comprobaciones de la seleccion de jugadores. Ademas de algun estilo mas añadido. Y guardado en session storage

--- js/main.js
+++ js/main.js
@@ -55,3 +55,44 @@ logicBoard.map(
         })
     }
 )
+
+
+
+
+//Recogida de datos inputs Seleccion de Jugadores y guardado en Session Storage
+
+let nombrePlayer1 = document.getElementById("namePl1");
+let nombrePlayer2 = document.getElementById("namePl2");
+
+
+botonAceptarPlayer1.addEventListener("click", () => {
+
+
+    if (nombrePlayer1.value === "" || nombrePlayer1.value.length < 3) {
+        document.getElementById("pMensaje1").innerHTML = ("Escribe un nombre que contenga <br> entre 3 y 12 caracteres")
+    } else {
+        sessionStorage.setItem("nombreJugador1", JSON.stringify(nombrePlayer1.value));
+        document.getElementById("labelNombre1").innerHTML = (nombrePlayer1.value);
+        document.getElementById("namePl1").value = (""); 
+    }
+})
+botonAceptarPlayer2.addEventListener("click", () => {
+
+    if (nombrePlayer2.value === "" || nombrePlayer2.value.length < 3) {
+        document.getElementById("pMensaje2").innerHTML = ("Escribe un nombre que contenga <br> entre 3 y 12 caracteres")
+    } else {
+        sessionStorage.setItem("nombreJugador2", JSON.stringify(nombrePlayer2.value));
+        document.getElementById("labelNombre2").innerHTML = (nombrePlayer2.value);
+        document.getElementById("namePl2").value = ("");   
+    }
+})
+
+if (sessionStorage.getItem("nombreJugador1" !== null) && sessionStorage.getItem("nombreJugador2" !== null)) {
+    
+    document.getElementById("botonAceptarSeleccion").style.visibility = "visible";
+}
+
+botonAceptarSeleccion.addEventListener("click", () => {  
+    window.open("../pages/juego.html", "_self");
+})
+
