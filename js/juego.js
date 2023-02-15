bc1c3addc4b17bc4eec7bf70024edbf34eab7957
jordijerico <jordijerico@gmail.com>
2023-02-15 18:52:41 +0100

    Quitar y volver a poner fichas implementando. Falta contador de 6, no esta limitado todavia pero gran avance

--- js/juego.js
+++ js/juego.js
@@ -40,10 +40,11 @@ document.getElementById('jugador1style').classList.add('pantallaPlayer1Styles');
 
 //Mapeo de las 'box' para escribir en ellas X y O en cada casilla
 let turn2 = true;
+let turnos = 6;
 logicBoard.map(box => {
     // Evento / funcion que comprueba los turnos y va pintando X o O cambiando el turno y bajando los turnos
     box.addEventListener("click", () => {
-        if ((box.innerHTML === "") && (turnPlayer1 > 0 || turnPlayer2 > 0) && turn) {
+        if ((box.innerHTML === "") && (turnPlayer1 > 0 || turnPlayer2 > 0) && turn && turnos > 0 ) {
             document.getElementById("turnosPlayer1").innerHTML = (`Turnos restantes: ${turnPlayer1}`);
             document.getElementById('jugador1style').classList.remove('pantallaPlayer1Styles');
             document.getElementById('jugador2style').classList.add('pantallaPlayer2Styles');
@@ -52,41 +53,41 @@ logicBoard.map(box => {
             turnPlayer1--;
             turn = !turn;
 
-        } else if ((box.innerHTML === "") && (turnPlayer1 > 0 || turnPlayer2 > 0) && !turn) {
-            turnPlayer2--;
+        } else if ((box.innerHTML === "") && (turnPlayer1 > 0 || turnPlayer2 > 0) && !turn && turnos > 0) {
             document.getElementById("turnosPlayer2").innerHTML = (`Turnos restantes: ${turnPlayer2}`);
             document.getElementById('jugador2style').classList.remove('pantallaPlayer2Styles');
             document.getElementById('jugador1style').classList.add('pantallaPlayer1Styles');
             box.innerHTML = (turn) ? "X" : "O";
             boardGame[box.id] = "O";
-            turnPlayer1--;
+            turnPlayer2--;
             turn = !turn;
 
-        } else if ((box.innerHTML === "X") && (turnPlayer1 === 0 && turnPlayer2 === 0) && turn) {
+        } else if ((box.innerHTML === "X") && (turnPlayer1 === 0 && turnPlayer2 === 0) && turn2) {
             console.log("1");
             box.innerHTML = "";
             boardGame[box.id] = "";
 
-        } else if (box.innerHTML === "" && turnPlayer1 === 0 && turn) {
+        } else if (box.innerHTML === "" && (turnPlayer1 === 0 && turnPlayer2 === 0) && turn2) {
             console.log("2");
             box.innerHTML = "X"
             boardGame[box.id] = "X";
-            turn = !turn;
+            turn2 = !turn2;
+            document.getElementById('jugador1style').classList.remove('pantallaPlayer1Styles');
+            document.getElementById('jugador2style').classList.add('pantallaPlayer2Styles');
         }
-        else if ((box.innerHTML === "O") && (turnPlayer1 === 0 && turnPlayer2 === 0) && !turn) {
+        else if ((box.innerHTML === "O") && (turnPlayer1 === 0 && turnPlayer2 === 0) && !turn2) {
             console.log("3");
             box.innerHTML = "";
             boardGame[box.id] = "";
 
-        } else if (box.innerHTML === "" && turnPlayer2 === 0 && !turn) {
+        } else if (box.innerHTML === "" && (turnPlayer2 === 0 && turnPlayer1 === 0) && !turn2) {
             console.log("4");
             box.innerHTML = "O"
             boardGame[box.id] = "O";
-            turn = !turn;
+            turn2 = !turn2;
+            document.getElementById('jugador2style').classList.remove('pantallaPlayer2Styles');
+            document.getElementById('jugador1style').classList.add('pantallaPlayer1Styles');
         }
-
-        console.log(boardGame);
-
 
         //COMPROBAR WINNER
         let comprobante;
@@ -116,7 +117,6 @@ const winnerCheck = () => {
         let [x, y, z] = chartWinner[i];
         if (boardGame[x] === boardGame[y] && boardGame[y] === boardGame[z] && boardGame[x] === boardGame[z]) {
             return boardGame[x];
-            ;
         }
     }
     return null;
